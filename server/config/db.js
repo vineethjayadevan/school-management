0d757120c78929eb6c95105242ae0daeb248d5b9
Vineeth <vineethv1998@mystemgps.com>
2026-02-22 16:24:37 +0530

    Fix Teacher portal issues: DB connection, layout, API queries

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
+        if (process.env.NODE_ENV === 'development') {
+            require('dns').setServers(['8.8.8.8']);
+        }
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
