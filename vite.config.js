54e55a0b03faa04085be8055b4ae59306b9f8ba1
vineeth v <vineethjay1998@gmail.com>
2026-03-10 17:17:32 +0530

    fix(client): increase workbox maximum cache size for PWA build

--- vite.config.js
+++ vite.config.js
@@ -31,6 +31,9 @@ export default defineConfig({
             purpose: 'any maskable'
           }
         ]
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5000000 // 5MB
       }
     })
   ],
